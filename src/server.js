a59c63cbf13d96eb63138f4b29f47ff471d1d26e
Anastasiia Savinova Occhiminuti <anastesie@hotmail.com>
2025-07-17 11:58:17 +0200

    HW-7

--- src/server.js
+++ src/server.js
@@ -11,6 +11,8 @@ import { notFoundHandler } from "./middlewares/notFoundHandler.js";
 import { errorHandler } from "./middlewares/errorHandler.js";
 import { UPLOAD_DIR } from './constants/index.js';
 
+import { swaggerDocs } from './middlewares/swaggerDocs.js';
+
 const PORT = Number(getEnvVar('PORT', 3000));
 
 export const setupServer = () => {
@@ -26,15 +28,16 @@ export const setupServer = () => {
     }));
 
     // app.use("/contacts", contactsRouter);
+  app.use('/uploads', express.static(UPLOAD_DIR));
+    app.use('/api-docs', swaggerDocs());
+    
     app.use(router);
     app.use(notFoundHandler);
     app.use(errorHandler);
-    
-    app.use('/uploads', express.static(UPLOAD_DIR));
-    
+
     app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
 }
 
 
-
+//OK
 
